4edca3427461aa6f0a731c739ba92f156bcc58fd
Korede Anunusor <160490663+Koryslick16@users.noreply.github.com>
2024-04-02 10:33:42 +0100

    Update assignment5.js

--- assignment5.js
+++ assignment5.js
@@ -7,20 +7,20 @@ let operation =  '+';
 
 switch(operation){
     case '+':
-        console.log("Addition", num1 + num2);
+        console.log( num1 + num2);
         break;
     case '-':
-        console.log("Subtraction", num1 - num2);
+        console.log( num1 - num2);
         break;
     case '/':
         if(num2 !== 0) {
-            console.log( "Division", num1 / num2);
+            console.log( num1 / num2);
         } else{
             console.log( "Error! Division by zero is not possible");
         }
         break;
     case '%':
-    console.log("Modulus", num1 % num2);
+    console.log( num1 % num2);
         break;
     default:
         console.log("Invalid Operation");
