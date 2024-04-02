94cd29c10e02bedfbd553f05cefb5230e3d9f8a2
Korede Anunusor <160490663+Koryslick16@users.noreply.github.com>
2024-04-02 10:24:21 +0100

    Update assignment11.js

--- assignment11.js
+++ assignment11.js
@@ -5,11 +5,11 @@ let age= 15;
 let score= '50%'
 
 
-if('age >= 16 && score >= 80%'){
+if(age >= 16 && score >= '80%'){
        console.log("Driver's License Approved.");
 }
-else if ('age < 16 && score >= 90%'){
-    console.log("Learner's permit granted");
+else if (age < 16 && score >= '90%'){
+    console.log("Learner's permit granted.");
 }
 else{
     console.log("Driver's license declined!");
